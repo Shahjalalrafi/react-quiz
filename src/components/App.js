2e5dedd26735ae5d5526c43e01e081b9f978ece3
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-10-01 17:15:17 +0600

    quiz section done

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,15 @@ import Layout from './Layout';
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
     <Layout>
       {/* <Home /> */}
-      {/* <Signup /> */}
-      <Login />
+      {/* <Signup />
+      <Login /> */}
+      <Quiz />
     </Layout>
   );
 }
