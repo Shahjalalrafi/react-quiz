f82aff2f3606a4faeb977367b2f4f5b9f857dc8f
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-10-01 17:39:43 +0600

    result page done

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
+import Result from "./pages/Results";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <Home /> */}
       {/* <Signup />
       <Login /> */}
-      <Quiz />
+      {/* <Quiz /> */}
+      <Result />
     </Layout>
   );
 }
