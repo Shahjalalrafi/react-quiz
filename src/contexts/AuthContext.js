634bbba5761d6c2be2765783bbd5302f409ee33b
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-10-02 21:55:48 +0600

    signup login logout implement

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,65 @@
+import React, { useContext, useEffect, useState } from "react"
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+
+import "../firebase"
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+    const [loading, setLoading] = useState(false)
+    const [currentUser, setCurrentUser] = useState()
+
+    useEffect(()=> {
+        const auth = useAuth()
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            setLoading(false);
+          });
+
+          return unsubscribe
+    }, [])
+
+    // SignUP
+    async function signUp(email, password, username) {
+        const auth = getAuth()
+        await createUserWithEmailAndPassword(auth, email, password)
+
+        // Update profile
+        await updateProfile(auth.currentUser, {
+            displayName: username
+        })
+        const user = auth.currentUser;
+        setCurrentUser({
+            ...user
+        })
+    }
+
+    // Login
+    function login(email, password) {
+        const auth = getAuth()
+        signInWithEmailAndPassword(auth, email, password)
+    }
+
+    // logout
+    function logout() {
+        const auth = getAuth()
+        return signOut(auth)
+    }
+
+    const value = {
+        currentUser,
+        signUp,
+        login,
+        logout
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
